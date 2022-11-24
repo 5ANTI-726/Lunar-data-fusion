63a282c43b05d0cbe068262d271005243edcbeda
5ANTI-726 <a00829556@itesm.mx>
2022-11-24 11:53:14 -0600

    File name checking and preprocessing update
    File names now are assured to include their channel (NIR or UVV) as a prefix and preprocessing is made slightly more efficient. Preprocessing avoids interpolating values for large swathes of non-image and only interpolates for dead pixels (both salt and pepper errors). All files are converted to their final size (recommended 420 x 420 pixels) after preprocessing.

--- workflow.py
+++ workflow.py
@@ -61,6 +61,8 @@ def interpolation_criteria(im,x,y):
         type = 'no edge'
 
     #Edge cases
+
+    #Edge cases
     elif x == 0 and y > 0 and y < height-1:
         up = im.getpixel((x,y+1))
         down = im.getpixel((x,y-1))
@@ -109,7 +111,7 @@ def interpolation_criteria(im,x,y):
         type = 'corner 1'
 
     #Return whether it meets criteria (one allowable dead neighbor)
-    if dead_neighbors <= 1:
+    if dead_neighbors <= 2:
         return (True,type)
     else:
         return (False,type)
@@ -129,21 +131,15 @@ def mosaic_create(origin, replace_mosaics):
 
                 width, height = im.size
                 dead_pixel_count = 0
-                fix_count = 0
                 for x in range(0,width):
                     for y in range(0,height):
-                        if im.getpixel((x,y)) <= 10:
+                        pixel = im.getpixel((x,y))
+                        if pixel <= 1 or pixel == 256:
                             dead_pixel_count += 1
-                            interpolate, pixeltype = interpolation_criteria(im,x,y)
-                            if interpolate:
-                                fix_count += 1
-                top = width*height - dead_pixel_count + fix_count
+                top = width*height - dead_pixel_count
                 bottom = width*height
-                if bottom != 0:
-                    quality_measure = top/bottom
-                else:
-                    quality_measure = 0
-                quality_matrix.append(quality_measure)
+
+                quality_matrix.append(top/bottom)
         return quality_matrix
 
     def contrast(source):
@@ -175,7 +171,7 @@ def mosaic_create(origin, replace_mosaics):
                     x = register[i]
                     temp.append(quadratic_map(a, c, d, x))
                 average = sum(temp)/len(temp)
-                averages.append(abs(average - 100))
+                averages.append(abs(average - 90))
 
             p = averages.index(min(averages)) + 116
             #print("P: " + str(p))
@@ -244,6 +240,7 @@ def mosaic_create(origin, replace_mosaics):
         #If the register is empty, return the original image
         return source, source, source
 
+    threshold = 1
     for item in os.listdir(origin):
         if item == "Original data and scripts":
             #print(origin + item)
@@ -253,7 +250,7 @@ def mosaic_create(origin, replace_mosaics):
                 if area[0] != ".":
                     for site_data in os.listdir(origin + item + area):
                         site_data += "/"
-                        if site_data[-11:] == "_processed/" and not os.exists(origin + item + area + site_data + "mosaic_1.png"):
+                        if site_data[-11:] == "_processed/" and not path.exists(origin + item + area + site_data + "mosaic_1.png"):
                             parent_directory = origin + item + area + site_data
                             destination_directory = parent_directory
 
@@ -276,18 +273,20 @@ def mosaic_create(origin, replace_mosaics):
 
                             for index in quality_order:
                                 #Add to canvas in order of quality.
-                                next_image_1, next_image_2, next_image_3 = contrast( Image.open( parent_directory + tiff_list[quality_order[index]] ) .resize((training_image_size,training_image_size)))
+                                current_mosaic_item = Image.open( parent_directory + tiff_list[quality_order[index]] )
+                                #current_mask = Image.open( parent_directory + tiff_list[quality_order[index]] + '.msk')
+                                next_image_1, next_image_2, next_image_3 = contrast(current_mosaic_item)
 
                                 for x in range(0,training_image_size):
                                     for y in range(0,training_image_size):
                                         new_pixel_1 = next_image_1.getpixel((x,y))
                                         new_pixel_2 = next_image_2.getpixel((x,y))
                                         new_pixel_3 = next_image_3.getpixel((x,y))
-                                        if new_pixel_1 > 10:
+                                        if new_pixel_1 > threshold:
                                             basis_1.putpixel((x,y), new_pixel_1)
-                                        if new_pixel_2 > 10:
+                                        if new_pixel_2 > threshold:
                                             basis_2.putpixel((x,y), new_pixel_2)
-                                        if new_pixel_3 > 10:
+                                        if new_pixel_3 > threshold:
                                             basis_3.putpixel((x,y), new_pixel_3)
 
                             if replace_mosaics:
@@ -343,8 +342,8 @@ def over80_filter(origin):
                                                     dead_pixel_count += 1
 
                                         completeness = (width*height - dead_pixel_count) / (width*height)
-                                        if completeness >= 0.8:
-                                            im.resize((training_image_size,training_image_size)).save(destination_directory + file[:-4] + '_over80.png')
+                                        if completeness >= 0.75:
+                                            im.save(destination_directory + file[:-4] + '_over80.png')
 
 def preprocessing(origin):
     #Input: The project's file; the "Original data and scripts" file has to be
@@ -362,27 +361,100 @@ def preprocessing(origin):
 
         #The interpolation transformation is applied based on the pixel type.
         #image variable is an Image object
-
+        #Max number of neighboring pixels that can be dead for the pixel to be interpolated.
+        threshold = 2
         if type == 'no edge':
-            image.putpixel((x,y), round((image.getpixel((x,y+1)) + image.getpixel((x,y-1)) + image.getpixel((x+1,y)) + image.getpixel((x-1,y)))/4))
+            neighbors = [image.getpixel((x,y+1)), image.getpixel((x,y-1)), image.getpixel((x+1,y)), image.getpixel((x-1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > (len(neighbors) - threshold):
+                image.putpixel((x,y), int(pixel/count))
         elif type == 'top edge':
-            image.putpixel((x,y), round((image.getpixel((x,y-1)) + image.getpixel((x+1,y)) + image.getpixel((x-1,y)))/3))
+            neighbors = [image.getpixel((x,y-1)), image.getpixel((x+1,y)), image.getpixel((x-1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > (len(neighbors) - threshold):
+                image.putpixel((x,y), int(pixel/count))
         elif type == 'bottom edge':
-            image.putpixel((x,y), round((image.getpixel((x,y+1))+ image.getpixel((x+1,y)) + image.getpixel((x-1,y)))/3))
+            neighbors = [image.getpixel((x,y+1)), image.getpixel((x+1,y)), image.getpixel((x-1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > (len(neighbors) - threshold):
+                image.putpixel((x,y), int(pixel/count))
         elif type == 'right edge':
-            image.putpixel((x,y), round((image.getpixel((x,y+1)) + image.getpixel((x,y-1)) + image.getpixel((x-1,y)))/3))
+            neighbors = [image.getpixel((x,y+1)), image.getpixel((x,y-1)), image.getpixel((x-1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > threshold:
+                image.putpixel((x,y), int(pixel/count))
         elif type == 'left edge':
-            image.putpixel((x,y), round((image.getpixel((x,y+1)) + image.getpixel((x,y-1)) + image.getpixel((x+1,y)))/3))
+            neighbors = [image.getpixel((x,y+1)), image.getpixel((x,y-1)), image.getpixel((x+1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > (len(neighbors) - threshold):
+                image.putpixel((x,y), int(pixel/count))
         elif type == 'corner 1':
-            image.putpixel((x,y), round((image.getpixel((x,y-1)) + image.getpixel((x-1,y)))/2))
+            neighbors = [image.getpixel((x,y-1)), image.getpixel((x-1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > (len(neighbors) - threshold):
+                image.putpixel((x,y), int(pixel/count))
         elif type == 'corner 2':
-            image.putpixel((x,y), round((image.getpixel((x,y-1)) + image.getpixel((x+1,y)))/2))
+            neighbors = [image.getpixel((x,y-1)), image.getpixel((x+1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > (len(neighbors) - threshold):
+                image.putpixel((x,y), int(pixel/count))
         elif type == 'corner 3':
-            image.putpixel((x,y), round((image.getpixel((x,y+1)) + image.getpixel((x+1,y)))/2))
+            neighbors = [image.getpixel((x,y+1)), image.getpixel((x+1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > (len(neighbors) - threshold):
+                image.putpixel((x,y), int(pixel/count))
         elif type == 'corner 4':
-            image.putpixel((x,y), round((image.getpixel((x,y+1)) + image.getpixel((x-1,y)))/2))
+            neighbors = [image.getpixel((x,y+1)), image.getpixel((x-1,y))]
+            pixel = 0
+            count = 0
+            for neighbor in neighbors:
+                if neighbor != 0:
+                    pixel += neighbor
+                    count += 1
+            if count > (len(neighbors) - threshold):
+                image.putpixel((x,y), int(pixel/count))
 
-    def dead_pixel_fix(im):
+    def dead_pixel_fix(im, mask):
         #Input: Image object.
         #Output: Image object with dead pixels fixed.
 
@@ -390,13 +462,17 @@ def preprocessing(origin):
         #criteria in last lines of interpolation_criteria() method and interpolation method
         #of interpolation() method.
         width, height = im.size
+        #Create original image object so you don't interpolate values for swaths
+        #of black areas.
+        original = im
         dead_pixel_count = 0
         fix_count = 0
         for x in range(0,width):
             for y in range(0,height):
-                if im.getpixel((x,y)) == 0:
+                pixel = im.getpixel((x,y))
+                if pixel == 0 or pixel == 256:
                     dead_pixel_count += 1
-                    interpolate, pixeltype = interpolation_criteria(im,x,y)
+                    interpolate, pixeltype = interpolation_criteria(original,x,y)
                     if interpolate:
                         fix_count += 1
                         interpolation(im,x,y,pixeltype)
@@ -430,9 +506,39 @@ def preprocessing(origin):
                                 for file in os.listdir(parent_directory):
                                     if file.endswith(".tif") or file.endswith(".png"):
                                         im = Image.open(parent_directory + file)
-                                        processed = dead_pixel_fix(dead_pixel_fix(dead_pixel_fix(dead_pixel_fix(im))))
+                                        mask = Image.open(parent_directory + file)
+                                        processed = dead_pixel_fix(dead_pixel_fix(dead_pixel_fix(dead_pixel_fix(im, mask), mask), mask), mask)
                                         processed.resize((training_image_size,training_image_size)).save(destination_directory + '/' + prefix + file[:-4] + '_processed.png')
 
+def prefix_labeller(origin):
+    for item in os.listdir(origin):
+        if item == "Original data and scripts":
+            #print(origin + item)
+            item += "/"
+            for area in  os.listdir(origin + item):
+                area += "/"
+                if area[0] != ".":
+                    for site_data in os.listdir(origin + item + area):
+                        site_data += "/"
+                        if (site_data[0:4] == "Near" or site_data[0:5] == "Ultra"):
+                            parent_directory = origin + item + area + site_data
+                            prefix = ''
+                            if site_data[0:4] == "Near":
+                                prefix = "NIR"
+                            elif site_data[0:5] == "Ultra":
+                                prefix = "UVV"
+
+                            for file in os.listdir(parent_directory):
+                                if file.endswith(".tif") or file.endswith(".png"):
+                                    if file[0:3] != "UVV" and file[0:3] != "NIR":
+                                    #if file[0:3] != "UVV" or file[0:3] != "NIR":
+                                        #im = Image.open(parent_directory + file)
+                                        #im.save(parent_directory + file[3:])
+                                        #os.remove(parent_directory + file)
+                                        os.rename(parent_directory + file, parent_directory + prefix + file)
+                                        #if file.find('mosaic') != -1:
+                                            #print("Incorrect label:" + file)
+                                            #print("Location:" + parent_directory)
 
 origin = "/Users/santi/Documents/Semestre 1-2-3/MR3038- Estancia de investigación/Proyectos/Data fusion in lunar environment/"
 training_image_size = 420
@@ -447,6 +553,11 @@ alarm(alarmV)
 print("")
 print("Begin creating mosaics of site data...")
 mosaic_create(origin, replace_mosaics)
+alarm(alarmV)
+
+print("")
+print("Make sure all files are correctly labelled, specially mosaic files.")
+prefix_labeller(origin)
 alarm(alarmV)
 
 print("")
